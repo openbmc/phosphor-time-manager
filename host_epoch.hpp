8de83e6e67cff2ab7c8edd48983b5a5a12c5ff18
Lei YU <mine260309@gmail.com>
2017-01-16 16:05:13 +0800

    Implement HostEpoch
    When host elapsed is set, the diff between the BmcTime and the value is
    saved to persistent storage;
    When host elapsed is get, return the BmcTime plus the diff as host's
    time.
    
    Add the unit test cases for HostEpoch, verify the logics.
    
    Change-Id: I8b01ff26e6716203d980aefd3d3f577d44713a91
    Signed-off-by: Lei YU <mine260309@gmail.com>

---
+++ host_epoch.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "epoch_base.hpp"
+
+#include <chrono>
+
+namespace phosphor
+{
+namespace time
+{
+
+class HostEpoch : public EpochBase
+{
+    public:
+        friend class TestHostEpoch;
+        HostEpoch(sdbusplus::bus::bus& bus,
+                  const char* objPath);
+
+        uint64_t elapsed() const override;
+        uint64_t elapsed(uint64_t value) override;
+    private:
+        std::chrono::microseconds offset;
+
+        // Store the offset in File System. Read back when starts.
+        static constexpr auto offsetFile = "/var/lib/obmc/saved_host_offset";
+
+        template <typename T>
+        static T readData(const char* fileName);
+        template <typename T>
+        static void writeData(const char* fileName, T&& data);
+};
+
+}
+}
