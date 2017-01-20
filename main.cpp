b70fe9170e6296e32a6709b573c0a80f43daab0b
Lei YU <mine260309@gmail.com>
2017-01-20 14:05:46 +0800

    Implement part of BmcEpoch
    Add BmcEpoch which inherits EpochBase and will handle times for BMC,
    it is partly implemented.
    
    Add unit test cases to test basic functions.
    
    Change-Id: Ia5e4d0f884156d238f3f84df490a2efbce43e89a
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <sdbusplus/bus.hpp>
 
 #include "config.h"
+#include "bmc_epoch.hpp"
 
 int main()
 {
     auto bus = sdbusplus::bus::new_default();
+    phosphor::time::BmcEpoch bmc(bus, OBJPATH_BMC);
 
     bus.request_name(BUSNAME);
 
