fc5c54f58f57d1f1a2bc72307b64c10bf40ff1b0
Lei YU <mine260309@gmail.com>
2017-01-20 13:49:39 +0800

    Add pgood signal handler.
    Change-Id: I7be4f7300aca3d1adc118eb3eb447c1e3734c89f
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- time_manager.cpp
+++ time_manager.cpp
@@ -5,7 +5,17 @@ namespace // anonymous
 constexpr auto MATCH_PROPERTY_CHANGE =
     "type='signal',interface='org.freedesktop.DBus.Properties',"
     "path='/org/openbmc/settings/host0',member='PropertiesChanged'";
+constexpr auto MATCH_PGOOD_CHANGE =
+    "type='signal',interface='org.freedesktop.DBus.Properties',"
+    "path='/org/openbmc/control/power0',member='PropertiesChanged'";
 
+// TODO: consider put the get properties related functions into a common place
+constexpr auto POWER_SERVICE = "org.openbmc.control.Power";
+constexpr auto POWER_PATH = "/org/openbmc/control/power0";
+constexpr auto POWER_INTERFACE = POWER_SERVICE;
+constexpr auto PROPERTY_INTERFACE = "org.freedesktop.DBus.Properties";
+constexpr auto METHOD_GET = "Get";
+constexpr auto PGOOD_STR = "pgood";
 }
 
 namespace phosphor
@@ -18,8 +28,10 @@ Manager::managedProperties = {"time_mode", "time_owner"};
 
 Manager::Manager(sdbusplus::bus::bus& bus)
     : bus(bus),
-      propertyChangeMatch(bus, MATCH_PROPERTY_CHANGE, onPropertyChanged, this)
+      propertyChangeMatch(bus, MATCH_PROPERTY_CHANGE, onPropertyChanged, this),
+      pgoodChangeMatch(bus, MATCH_PGOOD_CHANGE, onPgoodChanged, this)
 {
+    initPgood();
 }
 
 void Manager::addListener(PropertyChangeListner* listener)
@@ -27,12 +39,30 @@ void Manager::addListener(PropertyChangeListner* listener)
     listeners.insert(listener);
 }
 
+void Manager::initPgood()
+{
+    sdbusplus::message::variant<int> pgood = 0;
+    auto method = bus.new_method_call(POWER_SERVICE,
+                                      POWER_PATH,
+                                      PROPERTY_INTERFACE,
+                                      METHOD_GET);
+    method.append(PROPERTY_INTERFACE, PGOOD_STR);
+    auto reply = bus.call(method);
+    if (reply)
+    {
+        reply.read(pgood);
+    }
+
+    isHostOn = static_cast<bool>(pgood.get<int>());
+}
+
 void Manager::onPropertyChanged(const std::string& key,
                                 const std::string& value)
 {
     // TODO: Check pgood
     // If it's off, notify listners;
-    // If it's on, hold the values and store in persistent storage.
+    // If it's on, hold the values and store in persistent storage
+    // as requested time mode/owner.
     // And when pgood turns back to off, notify the listners.
     for (const auto listener : listeners)
     {
@@ -45,7 +75,7 @@ int Manager::onPropertyChanged(sd_bus_message* msg,
                                sd_bus_error* retError)
 {
     using properties = std::map < std::string,
-          sdbusplus::message::variant<int, std::string >>;
+          sdbusplus::message::variant<std::string> >;
     auto m = sdbusplus::message::message(msg);
     // message type: sa{sv}as
     std::string ignore;
@@ -57,6 +87,35 @@ int Manager::onPropertyChanged(sd_bus_message* msg,
         {
             static_cast<Manager*>(userData)
                 ->onPropertyChanged(item.first, item.second.get<std::string>());
+        }
+    }
+    return 0;
+}
+
+void Manager::onPgoodChanged(bool pgood)
+{
+    isHostOn = pgood;
+    // TODO: if host is off, check requested time_mode/owner:
+    // and notify the listeners if any.
+}
+
+int Manager::onPgoodChanged(sd_bus_message* msg,
+                            void* userData,
+                            sd_bus_error* retError)
+{
+    using properties = std::map < std::string,
+          sdbusplus::message::variant<int> >;
+    auto m = sdbusplus::message::message(msg);
+    // message type: sa{sv}as
+    std::string ignore;
+    properties props;
+    m.read(ignore, props);
+    for (const auto item : props)
+    {
+        if (item.first == PGOOD_STR)
+        {
+            static_cast<Manager*>(userData)
+                ->onPgoodChanged(static_cast<bool>(item.second.get<int>()));
         }
     }
     return 0;
