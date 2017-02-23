c6fe8693f72bdc673c25838c4c8a8a39a6cdea8b
Lei YU <mine260309@gmail.com>
2017-02-23 15:12:07 +0800

    Add pgood signal handler.
    Pgood indicate if host is on, which will be used to handle the time
    mode/owner changes.
    
    Change-Id: I7be4f7300aca3d1adc118eb3eb447c1e3734c89f
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- manager.hpp
+++ manager.hpp
@@ -25,6 +25,10 @@ class Manager
     public:
         friend class TestManager;
         explicit Manager(sdbusplus::bus::bus& bus);
+        Manager(const Manager&) = delete;
+        Manager& operator=(const Manager&) = delete;
+        Manager(Manager&&) = delete;
+        Manager& operator=(Manager&&) = delete;
 
         /** @brief Add a listener that will be called
           * when property is changed
@@ -38,14 +42,23 @@ class Manager
         /** @brief The match of settings property change */
         sdbusplus::bus::match::match propertyChangeMatch;
 
+        /** @brief The match of pgood change */
+        sdbusplus::bus::match::match pgoodChangeMatch;
+
         /** @brief The container to hold all the listeners */
         std::set<PropertyChangeListner*> listeners;
+
+        /** @brief The value to indicate if host is on */
+        bool hostOn = false;
 
         /** @brief The current time mode */
         Mode timeMode;
 
         /** @brief The current time owner */
         Owner timeOwner;
+
+        /** @brief Check if host is on and update hostOn variable */
+        void checkHostOn();
 
         /** @brief Get setting from settingsd service
          *
@@ -75,6 +88,12 @@ class Manager
         void onPropertyChanged(const std::string& key,
                                const std::string& value);
 
+        /** @brief Notified on pgood has changed
+         *
+         * @param[in] pgood - The changed pgood value
+         */
+        void onPgoodChanged(bool pgood);
+
         /** @brief The static function called on settings property changed
          *
          * @param[in] msg - Data associated with subscribed signal
@@ -84,6 +103,16 @@ class Manager
         static int onPropertyChanged(sd_bus_message* msg,
                                      void* userData,
                                      sd_bus_error* retError);
+
+        /** @brief Notified on pgood has changed
+         *
+         * @param[in] msg - Data associated with subscribed signal
+         * @param[in] userData - Pointer to this object instance
+         * @param[out] retError  - Not used but required with signal API
+         */
+        static int onPgoodChanged(sd_bus_message* msg,
+                                  void* userData,
+                                  sd_bus_error* retError);
 
         /** @brief Convert a string to enum Mode
          *
