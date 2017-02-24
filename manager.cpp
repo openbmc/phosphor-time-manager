a4d1e2aeb65287b5d7f4dbcc376d981cafab5831
Lei YU <mine260309@gmail.com>
2017-02-24 15:35:25 +0800

    Use gmock on property_change_listener
    1. Mock property_change_listener;
    2. Make unit test link against gmock
    3. Update TestManager to use mocked property change listener.
    4. Fix an issue found by the updated test case, that when the properties
    are changed during host is on, and changed back to the same as before,
    when host becomes off, the properties shall remain the same and listeners
    shall not be notified.
    
    Change-Id: I815b59cb23edfcac910c2a8c26ea5a71e872d92f
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- manager.cpp
+++ manager.cpp
@@ -242,20 +242,25 @@ void Manager::onPgoodChanged(bool pgood)
     }
     if (!requestedMode.empty())
     {
-        setCurrentTimeMode(requestedMode);
-        for (const auto& listener : listeners)
+        if (setCurrentTimeMode(requestedMode))
         {
-            listener->onModeChanged(timeMode);
+            for (const auto& listener : listeners)
+            {
+                listener->onModeChanged(timeMode);
+            }
+            updateNtpSetting(requestedMode);
         }
         updateNtpSetting(requestedMode);
         setRequestedMode({});
     }
     if (!requestedOwner.empty())
     {
-        setCurrentTimeOwner(requestedOwner);
-        for (const auto& listener : listeners)
+        if (setCurrentTimeOwner(requestedOwner))
         {
-            listener->onOwnerChanged(timeOwner);
+            for (const auto& listener : listeners)
+            {
+                listener->onOwnerChanged(timeOwner);
+            }
         }
         setRequestedOwner({});
     }
@@ -283,18 +288,36 @@ int Manager::onPgoodChanged(sd_bus_message* msg,
     return 0;
 }
 
-void Manager::setCurrentTimeMode(const std::string& mode)
+bool Manager::setCurrentTimeMode(const std::string& mode)
 {
-    log<level::INFO>("Time mode is changed",
-                     entry("MODE=%s", mode.c_str()));
-    timeMode = convertToMode(mode);
+    auto newMode = convertToMode(mode);
+    if (newMode != timeMode)
+    {
+        log<level::INFO>("Time mode is changed",
+                         entry("MODE=%s", mode.c_str()));
+        timeMode = newMode;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-void Manager::setCurrentTimeOwner(const std::string& owner)
+bool Manager::setCurrentTimeOwner(const std::string& owner)
 {
-    log<level::INFO>("Time owner is changed",
-                     entry("OWNER=%s", owner.c_str()));
-    timeOwner = convertToOwner(owner);
+    auto newOwner = convertToOwner(owner);
+    if (newOwner != timeOwner)
+    {
+        log<level::INFO>("Time owner is changed",
+                         entry("OWNER=%s", owner.c_str()));
+        timeOwner = newOwner;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 std::string Manager::getSettings(const char* value) const
