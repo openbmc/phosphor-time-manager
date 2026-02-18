99f1adcec20d708c4de9a41dbd0d14728b3a2fca
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2026-02-18 12:26:44 +0530

    Add command-line argument support for initial time sync mode
    Allow phosphor-time-manager to accept --mode=<ntp|manual> argument
    to set the initial time synchronization mode at startup.
    This enables systemd services to configure the time manager mode
    without D-Bus calls.
    
    Change-Id: I2c19e855c74f5602fa5d92a0f1b45afe19260325
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,11 +2,65 @@
 
 #include "bmc_epoch.hpp"
 #include "manager.hpp"
+#include "utils.hpp"
 
+#include <getopt.h>
+
+#include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
 
-int main()
+#include <string>
+
+void usage(const char* prog)
 {
+    lg2::error("Usage: {PROG} [options]", "PROG", prog);
+    lg2::error("Options:");
+    lg2::error("  --mode=<ntp|manual>  Set initial time synchronization mode");
+    lg2::error("                       ntp: Use NTP for time synchronization");
+    lg2::error("                       manual: Use manual time setting");
+    lg2::error("  -h, --help           Display this help message");
+}
+
+int main(int argc, char** argv)
+{
+    std::string initialMode;
+
+    // Parse command line arguments
+    static struct option long_options[] = {{"mode", required_argument, 0, 'm'},
+                                           {"help", no_argument, 0, 'h'},
+                                           {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c;
+
+    while ((c = getopt_long(argc, argv, "h", long_options, &option_index)) !=
+           -1)
+    {
+        switch (c)
+        {
+            case 'm':
+                initialMode = optarg;
+                if (initialMode != "ntp" && initialMode != "manual")
+                {
+                    lg2::error(
+                        "Invalid mode '{MODE}'. Must be 'ntp' or 'manual'",
+                        "MODE", initialMode);
+                    usage(argv[0]);
+                    return 1;
+                }
+                break;
+            case 'h':
+                usage(argv[0]);
+                return 0;
+            case '?':
+                usage(argv[0]);
+                return 1;
+            default:
+                break;
+        }
+    }
+
     auto bus = sdbusplus::bus::new_default();
     sd_event* event = nullptr;
 
@@ -26,6 +80,33 @@ int main()
 
     phosphor::time::Manager manager(bus);
     phosphor::time::BmcEpoch bmc(bus, objpathBmc, manager);
+
+    // Set initial mode if provided via command line
+    if (!initialMode.empty())
+    {
+        phosphor::time::Mode mode;
+        if (initialMode == "ntp")
+        {
+            mode = phosphor::time::Mode::NTP;
+            lg2::info("Setting initial time sync mode to NTP");
+        }
+        else if (initialMode == "manual")
+        {
+            mode = phosphor::time::Mode::Manual;
+            lg2::info("Setting initial time sync mode to Manual");
+        }
+
+        try
+        {
+            manager.setTimeMode(mode);
+        }
+        catch (const std::exception& e)
+        {
+            lg2::error("Failed to set initial mode: {ERROR}", "ERROR",
+                       e.what());
+            // Continue anyway - the manager will use default or persisted value
+        }
+    }
 
     bus.request_name(busname);
 
